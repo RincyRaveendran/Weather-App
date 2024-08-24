0a55cca3f0a36bde74b2412da77274e18d01f3d3
RincyRaveendran <rincyjiyash@gmail.com>
2024-08-24 12:48:56 +0530

    initial

---
+++ sw.js
@@ -0,0 +1,12 @@
+let day='Tuesday'
+
+switch(day){
+    case'monday':
+    console.log('1');
+    break
+    case"Tuesday":
+    console.log('2');
+    break
+    default:
+        console.log('no');
+}
